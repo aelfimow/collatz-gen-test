589bae3dbeab18f8d83f34771760e45a67cf9ec1
Alexander Elfimow <my.free.mail@gmx.de>
2019-04-28 22:18:02 +0200

    collatz-test: added files

---
+++ tests/test-collatz-calc.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <gtest/gtest.h>
+#include <collatz.h>
+
+TEST(COLLATZ_CALC, A)
+{
+    EXPECT_EQ(0, 0);
+}
---
+++ tests/test-collatz-length.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+#include <isbn-10.h>
+
+TEST(COLLATZ_LENGTH, A)
+{
+    EXPECT_EQ(0, 0);
+}

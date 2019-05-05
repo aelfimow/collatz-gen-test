eaf7337b21df56e3e0f38c9bd2785e19c6e417ce
Alexander Elfimow <my.free.mail@gmx.de>
2019-05-05 22:21:43 +0200

    test collatz calc: added test

--- tests/test-collatz-calc.cpp
+++ tests/test-collatz-calc.cpp
@@ -2,7 +2,65 @@
 #include <gtest/gtest.h>
 #include <collatz.h>
 
-TEST(COLLATZ_CALC, A)
+TEST(COLLATZ_CALC, N_19)
 {
-    EXPECT_EQ(0, 0);
+    auto n = collatz_calc(19);
+    EXPECT_EQ(n, 58);
+
+    n = collatz_calc(n);
+    EXPECT_EQ(n, 29);
+
+    n = collatz_calc(n);
+    EXPECT_EQ(n, 88);
+
+    n = collatz_calc(n);
+    EXPECT_EQ(n, 44);
+
+    n = collatz_calc(n);
+    EXPECT_EQ(n, 22);
+
+    n = collatz_calc(n);
+    EXPECT_EQ(n, 11);
+
+    n = collatz_calc(n);
+    EXPECT_EQ(n, 34);
+
+    n = collatz_calc(n);
+    EXPECT_EQ(n, 17);
+
+    n = collatz_calc(n);
+    EXPECT_EQ(n, 52);
+
+    n = collatz_calc(n);
+    EXPECT_EQ(n, 26);
+
+    n = collatz_calc(n);
+    EXPECT_EQ(n, 13);
+
+    n = collatz_calc(n);
+    EXPECT_EQ(n, 40);
+
+    n = collatz_calc(n);
+    EXPECT_EQ(n, 20);
+
+    n = collatz_calc(n);
+    EXPECT_EQ(n, 10);
+
+    n = collatz_calc(n);
+    EXPECT_EQ(n, 5);
+
+    n = collatz_calc(n);
+    EXPECT_EQ(n, 16);
+
+    n = collatz_calc(n);
+    EXPECT_EQ(n, 8);
+
+    n = collatz_calc(n);
+    EXPECT_EQ(n, 4);
+
+    n = collatz_calc(n);
+    EXPECT_EQ(n, 2);
+
+    n = collatz_calc(n);
+    EXPECT_EQ(n, 1);
 }
